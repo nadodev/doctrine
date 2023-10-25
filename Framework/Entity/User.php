cdea9ca7e4d5c19d50bf241d43b953b9c5574c6c
leonardo <programacao@studiioalpha.com.br>
2023-10-25 13:54:46 -0300

    feat: criado entity User

---
+++ Framework/Entity/User.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Framework\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+
+class User
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $username;
+
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+
+    /**
+     * @ORM\Column(type="string")
+    */
+    private $created_at;
+
+
+    public function __construct()
+    {
+        $dateTime = new \DateTimeImmutable();
+        $formattedDateTime = $dateTime->format('Y-m-d H:i:s');
+        $this->created_at = $formattedDateTime;
+    }
+  
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     */
+    public function setId($id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of username
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set the value of username
+     */
+    public function setUsername($username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     */
+    public function setPassword($password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of created_at
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Set the value of created_at
+     */
+    public function setCreatedAt($created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+}
