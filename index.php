d9bda0a63e55d6c15ed76437643f76f4ae9518cd
leonardo <programacao@studiioalpha.com.br>
2023-10-25 11:54:46 -0300

    removido as rotas antigas e ajustado a index

--- index.php
+++ index.php
@@ -6,39 +6,6 @@ use Framework\Http\Kernel;
 
 define('BASE_PATH', dirname(__DIR__.'/doctrine/'));
 
-
-// use Symfony\Component\HttpFoundation\Request;
-// use Symfony\Component\Routing\RequestContext;
-// use Symfony\Component\Routing\Matcher\UrlMatcher;
-// use Symfony\Component\Routing\RouteCollection;
-
-// $context = new RequestContext();
-// $context->fromRequest(Request::createFromGlobals());
-
-// $routes = new RouteCollection();
-
-// require __DIR__ . '/routes.php';
-
-// $matcher = new UrlMatcher($routes, $context);
-
-// $parameters = $matcher->match($context->getPathInfo());
-
-// $controller = $parameters['_controller'];
-// list($controllerClass, $controllerAction) = explode('::', $controller);
-
-// $controllerInstance = new $controllerClass();
-
-// $id = isset($parameters['id']) ? $parameters['id'] : null;
-
-// $controllerInstance->$controllerAction($id);
-
-/**
- * Creates a new Request object from the global variables and initializes a new Kernel object.
- * Handles the request with the Kernel object and sends the response.
- *
- * @param none
- * @return void
- */
     $request = Request::createFromGlobals();
 
     $kernel = new Kernel();
